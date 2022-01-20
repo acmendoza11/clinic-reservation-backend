c835778c770781b75258213ea0874ca038791362
acmendoza11 <ailger.cmendoza@gmail.com>
2022-01-20 20:33:43 +0800

    initial commit

---
+++ src/main/java/com/clinicreservationdemo/clinicreservation/ClinicreservationApplication.java
@@ -0,0 +1,13 @@
+package com.clinicreservationdemo.clinicreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicreservationdemo/clinicreservation/ClinicreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicreservationdemo.clinicreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
