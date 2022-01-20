831365255c3992f32af4ca8ee30cb9b52e426186
acmendoza11 <ailger.cmendoza@gmail.com>
2022-01-20 20:52:29 +0800

    reservation model

--- src/main/java/com/clinicreservationdemo/clinicreservation/model/Reservation.java
+++ src/main/java/com/clinicreservationdemo/clinicreservation/model/Reservation.java
@@ -1,5 +1,97 @@
 package com.clinicreservationdemo.clinicreservation.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Reservation {
 
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String email;
+    private String sex;
+    private String time;
+    private String concern;
+    private int doctorId;
+
+    public Reservation() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getConcern() {
+        return concern;
+    }
+
+    public void setConcern(String concern) {
+        this.concern = concern;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
 }
