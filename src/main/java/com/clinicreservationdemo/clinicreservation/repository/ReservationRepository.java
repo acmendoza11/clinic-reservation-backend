f3d055079395b69304c1b8311d968eb2d93e5d2b
acmendoza11 <ailger.cmendoza@gmail.com>
2022-01-20 20:54:42 +0800

    reservation model

--- src/main/java/com/clinicreservationdemo/clinicreservation/repository/ReservationRepository.java
+++ src/main/java/com/clinicreservationdemo/clinicreservation/repository/ReservationRepository.java
@@ -1,4 +1,10 @@
 package com.clinicreservationdemo.clinicreservation.repository;
 
-public class ReservationRepository {
+import com.clinicreservationdemo.clinicreservation.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
 }
